0ab73aaa492780ec165b2de1e7e2eaa4ab77537d
nielroger <nielrogerekka94@gmail.com>
2026-09-11 16:20:56 -0700

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        var res = 0;
+        foreach(var num in nums)
+            res = res ^ num;
+        
+        return res;
+    }
+}
