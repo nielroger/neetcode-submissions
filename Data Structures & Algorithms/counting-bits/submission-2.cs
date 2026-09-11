abd9af673f7592b47a8b14267f03fb00f5f0198e
nielroger <nielrogerekka94@gmail.com>
2026-09-11 16:28:06 -0700

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] CountBits(int n) {
+        
+        int[] ans = new int[n + 1];
+
+        for(int i = 1; i <= n; i++)
+        {
+            int count = ans[i >> 1];
+            if((i & 1) == 1)
+            {
+                count++;
+            }
+            ans[i] = count;
+        }
+
+        return ans;
+    }
+}
